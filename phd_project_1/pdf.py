517d9b20e13a73af6d5fc859adc90b13e9d5574a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-02-09 11:00:58 -0600

    Make the pdf script parametric

--- phd_project_1/pdf.py
+++ phd_project_1/pdf.py
@@ -1,32 +1,67 @@
-from os.path import abspath
+from os.path import abspath, isfile
 from pathlib import Path
+from pprint import pprint as print
+from typing import List, Literal
 
+import click
 import tiktoken
+from progress.bar import Bar
 from pypdf import PageObject, PdfReader
+from tiktoken.core import Encoding
 
 
-def main() -> None:
+def identifyAbsolutePath(path: Path) -> Path | Literal[False]:
+    absPath: Path = Path(abspath(path))
+
+    if isfile(path=absPath) and absPath.suffix == ".pdf":
+        return absPath
+
+    else:
+        return False
+
+
+def encodeText(text: str) -> List[int]:
+    encoder: Encoding = tiktoken.get_encoding("cl100k_base")
+    return encoder.encode(text=text)
+
+
+@click.command()
+@click.option(
+    "pdf",
+    "-p",
+    "--pdf",
+    help="Path to PDF file",
+    required=True,
+    type=Path,
+)
+def main(pdf: Path) -> None:
+    """
+    Simple function to extract all text from a PDF and print it to the console.
+    """
+
+    pdfAbsPath: Path | Literal[False] = identifyAbsolutePath(path=pdf)
+
+    if pdfAbsPath == False:
+        print("Invalid input. Please use a valid path to a PDF file.")
+        exit(1)
+
     text: str = ""
 
-    pdfFile: Path = Path(
-        abspath(
-            path="/home/nicholas/Zotero/storage/84HG6PHM/Kwon et al. - 2022 - A Fast Post-Training Pruning Framework for Transfo.pdf"
-        )
-    )
+    pdfReader: PdfReader = PdfReader(stream=pdfAbsPath, strict=False)
 
-    pdfReader: PdfReader = PdfReader(stream=pdfFile, strict=False)
+    pageCount: int = len(pdfReader.pages)
 
-    page: PageObject
-    for page in pdfReader.pages:
-        rawContent: str = page.extract_text()
-        content: str = rawContent.strip().replace("\n", " ").lower()
-        text = text + content
+    with Bar("Extracting text from PDF document...", max=pageCount) as bar:
+        page: PageObject
+        for page in pdfReader.pages:
+            rawContent: str = page.extract_text()
+            content: str = rawContent.strip().replace("\n", " ").lower()
+            text = text + content
+            bar.next()
 
-    enc = tiktoken.get_encoding("cl100k_base")
-    test = enc.encode(text=text)
+    encodedText: List[int] = encodeText(text=text)
 
-    print(test)
-    print(len(test))
+    print(text)
 
 
 if __name__ == "__main__":
